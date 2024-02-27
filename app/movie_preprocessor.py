185c30f1fdd891881e8c4913e95b2a8086b88501
ylose <heungeob1003@gmail.com>
2024-02-27 13:41:52 +0900

    merge 함수를 이용해서 movies.csv와 links.csv 파일 결합

--- app/movie_preprocessor.py
+++ app/movie_preprocessor.py
@@ -3,4 +3,10 @@ import requests
 
 if __name__ == "__main__":
     movies_df = pd.read_csv('data/movies.csv')
-    print(movies_df)
+    # id 를 문자로 인식하도록 type을 변경한다.
+    movies_df['movieId'] = movies_df['movieId'].astype(str)
+    links_df = pd.read_csv('data/links.csv', dtype=str)
+    merged_df = movies_df.merge(
+        links_df, on='movieId', how='left')  # movies_df를 기준으로 merge
+    print(merged_df)
+    print(merged_df.columns)
