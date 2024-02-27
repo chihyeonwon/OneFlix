315442bc8f1613988c2200d6eb58b9677e31d8eb
ylose <heungeob1003@gmail.com>
2024-02-27 14:20:10 +0900

    agg()를 이용하여 rating_count, rating_avg 칼럼 추가

--- app/movie_preprocessor.py
+++ app/movie_preprocessor.py
@@ -6,6 +6,18 @@ def add_url(row):
     return f"http://www.imdb.com/title/tt{row}/"
 
 
+def add_rating(df):
+    ratings_df = pd.read_csv('data/ratings.csv')
+    ratings_df['movieId'] = ratings_df['movieId'].astype(str)
+    agg_df = ratings_df.groupby('movieId').agg(
+        rating_count=('rating', 'count'),
+        rating_avg=('rating', 'mean')
+    ).reset_index()
+
+    rating_added_df = df.merge(agg_df, on='movieId')
+    return rating_added_df
+
+
 if __name__ == "__main__":
     movies_df = pd.read_csv('data/movies.csv')
     # id 를 문자로 인식하도록 type을 변경한다.
@@ -14,5 +26,5 @@ if __name__ == "__main__":
     merged_df = movies_df.merge(
         links_df, on='movieId', how='left')  # movies_df를 기준으로 merge
     merged_df['url'] = merged_df['imdbId'].apply(lambda x: add_url(x))
-    print(merged_df)
-    print(merged_df.iloc[1, :])
+    result_df = add_rating(merged_df)
+    print(result_df)
