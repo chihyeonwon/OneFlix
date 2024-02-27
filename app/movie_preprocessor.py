9d3436bb0e31563e1e75aff46d04eee3368a87d2
ylose <heungeob1003@gmail.com>
2024-02-27 13:54:40 +0900

    url 컬럼 추가

--- app/movie_preprocessor.py
+++ app/movie_preprocessor.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import requests
 
+
+def add_url(row):
+    return f"http://www.imdb.com/title/tt{row}/"
+
+
 if __name__ == "__main__":
     movies_df = pd.read_csv('data/movies.csv')
     # id 를 문자로 인식하도록 type을 변경한다.
@@ -8,5 +13,6 @@ if __name__ == "__main__":
     links_df = pd.read_csv('data/links.csv', dtype=str)
     merged_df = movies_df.merge(
         links_df, on='movieId', how='left')  # movies_df를 기준으로 merge
+    merged_df['url'] = merged_df['imdbId'].apply(lambda x: add_url(x))
     print(merged_df)
-    print(merged_df.columns)
+    print(merged_df.iloc[1, :])
