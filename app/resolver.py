585073c798e6baac5e43f61701e799202c25b43f
ylose <heungeob1003@gmail.com>
2024-02-29 13:48:42 +0900

    장르별 영화를 반환하는 random_genres_items 메서드 작성

--- app/resolver.py
+++ app/resolver.py
@@ -8,3 +8,12 @@ def random_items():
     movies_df = movies_df.fillna('')  # 공백을 채워준다
     result_items = movies_df.sample(n=10).to_dict("records")
     return result_items
+
+
+def random_genres_items(genre):
+    movies_df = pd.read_csv(item_fname)
+    genre_df = movies_df[movies_df['genres'].apply(
+        lambda x: genre in x.lower())]
+    genre_df = genre_df.fillna('')  # 공백을 채워준다
+    result_items = genre_df.sample(n=10).to_dict("records")
+    return result_items
